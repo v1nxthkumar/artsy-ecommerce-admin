ec04101be0f9b53f567ee5cb36b1a1ba4ea4410e
Vinothkumar <vinothkumar.msc.cs@gmail.com>
2025-12-07 04:15:53 +0530

    final commit

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -82,10 +82,8 @@ import collection_icon from './collection_icon.png'
 import heart from './heart.png'
 import google from './google.png'
 import WLogo from './White.png'
-import video from './v1.mp4'
 
 export const assets = {
-    video,
     logo,
     WLogo,
     hero_img,
@@ -133,7 +131,7 @@ export const products = [
         name: "Men Round Neck Pure Cotton T-shirt",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 200,
-        image: [p_img2_1,p_img2_2,p_img2_3,p_img2_4],
+        image: [p_img2_1, p_img2_2, p_img2_3, p_img2_4],
         category: "Men",
         subCategory: "Topwear",
         sizes: ["M", "L", "XL"],
